97ed28edf9275245a570a5aac4d20ddb3203f0c2
jamesncox <jamesncox@yahoo.com>
2019-10-07 16:29:53 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+const createTaskForm = document.querySelector("#create-task-form");
+const newTaskDescription = document.querySelector("#new-task-description");
+const myTodos = document.querySelector("#tasks");
+
+// document.addEventListener("DOMContentLoaded", () => {
+//   // your code here
+// });
+
+createTaskForm.addEventListener("submit", onSubmit);
+
+function onSubmit(e) {
+  e.preventDefault();
+
+  // console.log(newTaskDescription.value);
+
+  const li = document.createElement("li");
+  // li.appendChild(document.createTextNode(`${newTaskDescription.value}`));
+  li.textContent = newTaskDescription.value;
+
+  myTodos.appendChild(li);
+}
